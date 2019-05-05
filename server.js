21b3879fc360a44cde318c3c8c4515541452e4c3
tiffanykeller <bakertiffany133@yahoo.com>
2019-05-04 20:54:12 -0500

    updated port

--- server.js
+++ server.js
@@ -33,10 +33,9 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 
 // Start the server
-app.listen(PORT, function() {
-  console.log(
-    "Listening on port 3000",
+app.listen(process.env.PORT || 3000, function(){
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
     
     
-  );
-});
+  
